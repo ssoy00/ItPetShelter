c50e57c861416a5d1ca0c208901d9821dbfb07a8
NamGwiYeon <earkite.n@gmail.com>
2024-06-12 17:59:33 +0900

    first commit

---
+++ src/main/java/com/itpetshelter/itpetshelter/ItPetShelterApplication.java
@@ -0,0 +1,13 @@
+package com.itpetshelter.itpetshelter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItPetShelterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItPetShelterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/itpetshelter/itpetshelter/ItPetShelterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itpetshelter.itpetshelter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItPetShelterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
