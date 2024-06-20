56954776c4082d6dc8df17be0b39412d1c411cef
NamGwiYeon <earkite.n@gmail.com>
2024-06-20 16:00:55 +0900

    domain volunteer vno 번호자동 고침

--- src/main/java/com/itpetshelter/itpetshelter/domain/Volunteer.java
+++ src/main/java/com/itpetshelter/itpetshelter/domain/Volunteer.java
@@ -14,6 +14,7 @@ import java.time.LocalDateTime;
 @Entity
 public class Volunteer extends BaseEntity{
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int Vno;
 
     @ManyToOne
