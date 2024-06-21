0fa4af64acf38b729f945b27cdee1d26e6c5ea79
kimsoyoung <ggomag3243@gmail.com>
2024-06-21 20:25:17 +0900

    오류 해결해야함...

---
+++ src/main/java/com/itpetshelter/itpetshelter/service/ReservationService.java
@@ -0,0 +1,7 @@
+package com.itpetshelter.itpetshelter.service;
+
+import com.itpetshelter.itpetshelter.domain.Reservation;
+
+public interface ReservationService {
+    void saveReservation(Reservation reservation);
+}
