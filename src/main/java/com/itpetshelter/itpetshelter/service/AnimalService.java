5e68e086f209839d98f658b32aa9dd766f991fc6
Kimminsu <kmsa0202@gmail.com>
2024-06-21 11:05:02 +0900

    서비스 -완-, 컨트롤러 1차

--- src/main/java/com/itpetshelter/itpetshelter/service/AnimalService.java
+++ src/main/java/com/itpetshelter/itpetshelter/service/AnimalService.java
@@ -1,5 +1,6 @@
 package com.itpetshelter.itpetshelter.service;
 
+import com.itpetshelter.itpetshelter.domain.Animal;
 import com.itpetshelter.itpetshelter.dto.AnimalDTO;
 
 import java.util.List;
@@ -8,4 +9,5 @@ public interface AnimalService {
 
     List<AnimalDTO> getAllAnimals();
     AnimalDTO getAnimalById(Long id);
+    AnimalDTO entityToDTO(Animal animal);
 }
