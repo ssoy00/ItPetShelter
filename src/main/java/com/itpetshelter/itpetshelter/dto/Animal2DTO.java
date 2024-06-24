193f816cd3600658d20179fd20356ef008a5823c
NamGwiYeon <earkite.n@gmail.com>
2024-06-24 10:57:29 +0900

    list, 상세페이지 수정

---
+++ src/main/java/com/itpetshelter/itpetshelter/dto/Animal2DTO.java
@@ -0,0 +1,39 @@
+package com.itpetshelter.itpetshelter.dto;
+
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Animal2DTO {
+
+    private Long Ano;
+
+    @NotEmpty
+    private String Atype;
+
+    @NotEmpty
+    private String Aname;
+
+
+    private Long Aage;
+    private Boolean Agender;
+    private Boolean Aneutered;
+    private Boolean Adisease;
+
+    @NotEmpty
+    private String Alocate;
+
+    private String Alocatename;
+
+    //첨부 파일 이름들
+    private List<String> fileNames;
+}
