b26e3ba665ebfcb008819856daae496989b51d8c
LeeHanSol <hslee4954@gmail.com>
2024-06-26 13:57:24 +0900

    매니져 레파지토리 시큐리티까지 작업함 아픙로 컨트롤러 예정

--- src/main/java/com/itpetshelter/itpetshelter/domain/Manager.java
+++ src/main/java/com/itpetshelter/itpetshelter/domain/Manager.java
@@ -3,6 +3,9 @@ package com.itpetshelter.itpetshelter.domain;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @Builder
 @Getter
 @Setter
@@ -12,22 +15,24 @@ import lombok.*;
 @Entity
 public class Manager {
     @Id
-    @GeneratedValue(strategy= GenerationType.IDENTITY)
-    private Long Mno;
+//    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private String Mid;
 
     @ManyToOne
     @JoinColumn(name = "Sno")
     private Shelter shelter;
-
     private String Mpw;
     private String Mname;
-    private String Mid;
 
     // 멤버를 조회시 roleSet 를 같이 조회를 하기.
-//    @ElementCollection(fetch = FetchType.LAZY)
-//    @Builder.Default
-//    private Set<MemberRole> roleSet = new HashSet<>();
+    @ElementCollection(fetch = FetchType.LAZY)
+    @Builder.Default
+    private Set<MemberRole> roleSet = new HashSet<>();
 
+    public void addRole(MemberRole memberRole) {
+        this.roleSet.add(memberRole);
+
+    }
 
 
 }
--- src/main/java/com/itpetshelter/itpetshelter/security/CustomUserDetailsService.java
+++ src/main/java/com/itpetshelter/itpetshelter/security/CustomUserDetailsService.java
@@ -1,9 +1,11 @@
 package com.itpetshelter.itpetshelter.security;
 
-
 import com.itpetshelter.itpetshelter.domain.Consumer;
+import com.itpetshelter.itpetshelter.domain.Manager;
 import com.itpetshelter.itpetshelter.repository.ConsumerRepository;
+import com.itpetshelter.itpetshelter.repository.ManagerRepository;
 import com.itpetshelter.itpetshelter.security.dto.ConsumerSecurityDTO;
+import com.itpetshelter.itpetshelter.security.dto.ManagerSecurityDTO;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -19,87 +21,60 @@ import java.util.stream.Collectors;
 
 @Log4j2
 @Service
-
 public class CustomUserDetailsService implements UserDetailsService {
 
-    // 스프링 시큐리티 설정 클래스에 빈으로 등록한 인스턴스 주입하기.
     private PasswordEncoder passwordEncoder;
 
-    // MemberRepository 넣기.
     @Autowired
     private ConsumerRepository consumerRepository;
 
-    //생성자로 주입하기.
+    @Autowired
+    private ManagerRepository managerRepository;
+
     public CustomUserDetailsService() {
         this.passwordEncoder = new BCryptPasswordEncoder();
     }
 
-    // 로그인 로직 처리시, 여기를 반드시 거쳐 감.
-    // username : 로그인한 유저이름.
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        log.info("CustomUserDetailsService loadUserByUsername 확인 : "+ username);
+        log.info("CustomUserDetailsService loadUserByUsername 확인 : " + username);
 
-        //User, 스프링 시큐리티에서 제공하는 기본 클래스,
-        // 사용자 가 만든 User 가 아니다. 우리는 Member 사용할 예정.
+        Optional<Consumer> consumerResult = consumerRepository.getWithRoles(username);
 
-        //더미 데이터 임시 테스트
-//        UserDetails userDetails = User.builder()
-//                .username("lsy2")
-//                .password(passwordEncoder.encode("123456"))
-//                // 더미 데이터, 더미 권한.
-//                .authorities("ROLE_USER")
-////                .authorities("ROLE_ADMIN")
-//                .build();
-        // 로그인 한 유저명으로, 디비에서 검색을함.
-        Optional<Consumer> result = consumerRepository.getWithRoles(username);
-
-
-
-        if(result.isEmpty()){
-
-
-            //Manager 하나 더 추가.
-        Optional<Consumer> result2 = consumerRepository.getWithRoles(username);
-
-
-            if(result2.isEmpty()){
-                //예외 처리.
-                throw new UsernameNotFoundException("유저가 존재하지 않습니다");
-            }
+        if (consumerResult.isPresent()) {
+            Consumer consumer = consumerResult.get();
+            ConsumerSecurityDTO consumerSecurityDTO = new ConsumerSecurityDTO(
+                    consumer.getCid(),
+                    consumer.getCpw(),
+                    consumer.getEmail(),
+                    consumer.isDel(),
+                    false,
+                    consumer.getUuid(),
+                    consumer.getFileName(),
+                    consumer.getRoleSet().stream().map(
+                            memberRole -> new SimpleGrantedAuthority("ROLE_" + memberRole.name())
+                    ).collect(Collectors.toList())
+            );
+            log.info("CustomUserDetailsService loadUserByUsername consumerSecurityDTO 확인 :" + consumerSecurityDTO);
+            return consumerSecurityDTO;
         }
-        // 디비에 해당 유저가 있다면, 이어서 로그인 처리하기.
-        Consumer consumer = result.get();
-        // 디비에서 Manager  가져오기
-//        Consumer consumer = result.get();
 
-        // entity -> dto(UserDetails 타입), MemberSecurityDTO
-        // 권한 관련 커스텀 마이징 하기 어려워서 -> 기존 A a = new A();
-//        MemberSecurityDTO memberSecurityDTO = MemberSecurityDTO.builder()
-//                .mid(member.getMid())
-//                .mpw(member.getMpw())
-//                .email(member.getEmail())
-//                .del(member.isDel())
-//                .social(false)
-//                .
-//                .build();
-        // consumer 로
-        // 1) ConsumerSecurityDTO, 매니저 정보도 받을 수 있게 추가.
-        // 2) 분기문, ConsumerSecurityDTO 처럼, 매니저 엔티티가 받을 수 있게.
-        ConsumerSecurityDTO consumerSecurityDTO = new ConsumerSecurityDTO(
-                consumer.getCid(),
-                consumer.getCpw(),
-                consumer.getEmail(),
-                consumer.isDel(),
-           false,
-                consumer.getUuid(),
-                consumer.getFileName(),
-                consumer.getRoleSet().stream().map(
-                   memberRole -> new SimpleGrantedAuthority("ROLE_"+ memberRole.name())
-           ).collect(Collectors.toList())
-        );
-        log.info("CustomUserDetailsService loadUserByUsername memberSecurityDTO 확인 :" + consumerSecurityDTO);
+        Optional<Manager> managerResult = managerRepository.getWithRoles(username);
 
-        return consumerSecurityDTO;
+        if (managerResult.isPresent()) {
+            Manager manager = managerResult.get();
+            ManagerSecurityDTO managerSecurityDTO = new ManagerSecurityDTO(
+                    manager.getMid(),
+                    manager.getMpw(),
+                    manager.getMname(),
+                    manager.getRoleSet().stream().map(
+                            memberRole -> new SimpleGrantedAuthority("ROLE_" + memberRole.name())
+                    ).collect(Collectors.toList())
+            );
+            log.info("ManagerDetailsService loadUserByUsername managerSecurityDTO 확인 :" + managerSecurityDTO);
+            return managerSecurityDTO;
+        }
+
+        throw new UsernameNotFoundException("유저가 존재하지 않습니다");
     }
 }
