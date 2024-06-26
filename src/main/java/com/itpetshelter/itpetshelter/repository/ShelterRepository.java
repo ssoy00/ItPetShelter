2fff27b7152ad1a096d7a5b6cdea71a8e9cd66a8
Kimminsu <kmsa0202@gmail.com>
2024-06-26 10:55:21 +0900

    insert (animalList22 -> animalInsert) 추가, 기능 작동 확인

--- src/main/java/com/itpetshelter/itpetshelter/repository/ShelterRepository.java
+++ src/main/java/com/itpetshelter/itpetshelter/repository/ShelterRepository.java
@@ -4,5 +4,8 @@ package com.itpetshelter.itpetshelter.repository;
 import com.itpetshelter.itpetshelter.domain.Shelter;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ShelterRepository extends JpaRepository<Shelter, Integer> {
+    Optional<Shelter> findBySnameAndSlocate(String Sname, String Slocate);
 }
